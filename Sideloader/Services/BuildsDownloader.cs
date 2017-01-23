335970b1f37ad57c96ba6021e526832e6c46d76a
Bogdan Bujdea <bogdan@thewindev.net>
2017-01-23 02:01:59 +0200

    Added html agility pack

--- Sideloader/Services/BuildsDownloader.cs
+++ Sideloader/Services/BuildsDownloader.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
+using HtmlAgilityPack;
 using Sideloader.Settings;
 using Sideloader.ViewModels;
 
@@ -24,15 +28,76 @@ namespace Sideloader.Services
                 if(report.IsSuccessful == false)
                     throw new UnauthorizedAccessException();
             }
+            string htmlText = string.Empty;
 
             if (WebClient.HttpClient != null)
             {
                 var packagesResponse = await WebClient.HttpClient.GetAsync("http://builds.showpad.com/showpad/showpad/windows?allBranches=1&download=1");
-                var html = await packagesResponse.Content.ReadAsStringAsync();
-                if (html.Contains("action=\"login.php\""))
+                if (packagesResponse.IsSuccessStatusCode == false)
+                {
+                    throw new Exception(packagesResponse.StatusCode.ToString());
+                }
+                htmlText = await packagesResponse.Content.ReadAsStringAsync();
+                if (htmlText.Contains("action=\"login.php\""))
                     throw new UnauthorizedAccessException();
             }
-            return new List<Build>();
+            return ExtractBuildsFromHtml(htmlText);
         }
+
+        private List<Build> ExtractBuildsFromHtml(string htmlText)
+        {
+            throw new NotImplementedException();
+        }
+
+        /* private List<Build> ExtractBuildsFromHtml(string htmlText)
+        {
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(htmlText);
+            foreach (HtmlNode linkNode in htmlDocument.DocumentNode.SelectNodes("//a[@href]"))
+            {
+                Debug.WriteLine(linkNode.Name + " : " + linkNode.Attributes);
+                var attr =
+                    linkNode.Attributes.FirstOrDefault(a => a.Name == "href" && a.Value.Contains("SWA-"));
+                if (attr != null)
+                {
+                    if (IsValidUri(attr.Value))
+                    {
+                        var urlDecode = WebUtility.HtmlDecode(attr.Value);
+                        downloadLinks.Add(new AppPackage(urlDecode));
+                        Debug.WriteLine(attr.Name + " : " + attr.Value);
+                    }
+                }
+            }
+        }*/
+
+        private List<AppPackage> ExtractLinksFromHtml(string htmlText, string ticketNumber)
+        {
+            var downloadLinks = new List<AppPackage>();
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(htmlText);
+            foreach (HtmlNode linkNode in htmlDocument.DocumentNode.SelectNodes("//a[@href]"))
+            {
+                Debug.WriteLine(linkNode.Name + " : " + linkNode.Attributes);
+                var attr =
+                    linkNode.Attributes.FirstOrDefault(a => a.Name == "href" && a.Value.Contains("SWA-" + ticketNumber));
+                if (attr != null)
+                {
+                    if (IsValidUri(attr.Value))
+                    {
+                        var urlDecode = WebUtility.HtmlDecode(attr.Value);
+                        downloadLinks.Add(new AppPackage(urlDecode));
+                        Debug.WriteLine(attr.Name + " : " + attr.Value);
+                    }
+                }
+            }
+            return downloadLinks;
+        }
+
+        private bool IsValidUri(string uri)
+        {
+            return Uri.IsWellFormedUriString(uri, UriKind.Absolute);
+        }
+
+
     }
 }
